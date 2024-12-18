21d7fe23a17ae5e5a872ce434a96396de38cd52d
rishikanaujia <python.institute.llp@gmail.com>
2024-12-19 00:21:21 +0530

    optimise config.py

--- app/config.py
+++ app/config.py
@@ -1,5 +1,5 @@
-import configparser
 import os
+import configparser
 from pydantic import BaseModel
 
 
@@ -14,13 +14,18 @@ class AppConfig(BaseModel):
     def from_env():
         """Load configuration from environment or .ini file."""
         config = configparser.ConfigParser()
+
+        # Reading the config file from the app's configs directory
         config.read(os.path.join(os.path.dirname(__file__), "configs/app_config.ini"))
-        env = os.environ.get("FLASK_ENV", "dev")
+
+        # Fallback to environment variables
+        env = os.environ.get("FLASK_ENV", config.get("DEFAULT", "env", fallback="dev"))
+
         return AppConfig(
-            ENV=os.getenv("ENV", config.get(env, "env", fallback="dev")),
-            DEBUG=os.getenv("DEBUG", config.getboolean(env, "debug", fallback="false")),
+            ENV=os.getenv("ENV", config.get(env, "env", fallback=env)),
+            DEBUG=os.getenv("DEBUG", config.getboolean(env, "debug", fallback=False)),
             HOST=os.getenv("HOST", config.get(env, "host", fallback="127.0.0.1")),
-            PORT=os.getenv("PORT", config.getint(env, "port", fallback=5000)),
+            PORT=int(os.getenv("PORT", config.get(env, "port", fallback=5000))),
             LOGGING_LEVEL=os.getenv(
                 "LOGGING_LEVEL", config.get(env, "logging_level", fallback="INFO")
             ),
